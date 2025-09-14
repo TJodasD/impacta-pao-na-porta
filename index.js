7e9f6988069977ea9e378c2f2b0fdb11c41e92a5
vzavan <victorsfzavan@gmail.com>
2025-09-14 14:27:21 -0300

    changing the port for the backend

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ require("dotenv").config()
 
 const app = require("./app")
 const api = process.env.SUPABASE_API_KEY
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 8000
 
 app.listen(port, () => {
     console.log(`Rodando na porta ${port}`)
